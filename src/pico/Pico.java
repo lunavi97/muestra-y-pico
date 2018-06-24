d4dde152b351d649cd451fb8f4ee31b5d194284a
Luciano Nahuel Vitale <lunavi97@gmail.com>
2018-06-24 06:42:09 -0300

    #2 WIP - Obtener el mayor entero usando ordenamiento o cola de prioridad

--- src/pico/Pico.java
+++ src/pico/Pico.java
@@ -1,5 +1,23 @@
 package pico;
 
-public class Pico {
+import java.util.ArrayList;
+import java.util.PriorityQueue;
 
+public class Pico {
+        
+    public int obtener(PriorityQueue<Integer> pq) {
+        return pq.peek();
+    }
+    
+    public int obtener(int[] vec) {
+        PriorityQueue<Integer> pq = new PriorityQueue<Integer>((x, y) -> y - x);
+        for(int num : vec) pq.add(num);
+        
+        return obtener(pq);
+    }
+    
+    public int obtener(ArrayList<Integer> al) {
+        al.sort((x, y) -> y - x);
+        return al.get(0);
+    }
 }
--- test/pico/PicoTests.java
+++ test/pico/PicoTests.java
@@ -1,14 +1,46 @@
 package pico;
 
-import static org.junit.Assert.*;
+import java.util.ArrayList;
+import java.util.PriorityQueue;
 
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class PicoTests {
+    
+    Pico pico;
 
+    @Before
+    public void SetUp() {
+        pico = new Pico();
+    }
+    
     @Test
-    public void test() {
-        fail("Not yet implemented");
+    public void conColaDePrioridad() {
+        PriorityQueue<Integer> pq = new PriorityQueue<Integer>((x, y) -> y - x);
+        pq.add(2);
+        pq.add(3);
+        pq.add(8);
+        pq.add(5);
+        Assert.assertEquals(8, pico.obtener(pq));
+    }
+    
+    @Test
+    public void conVector() {
+        int[] vec = {4, 6, 8, 10, 7, 3, 2, 1};
+        Assert.assertEquals(10, pico.obtener(vec));
+    }
+    
+    @Test
+    public void conLista() {
+        ArrayList<Integer> al = new ArrayList<Integer>();
+        al.add(5);
+        al.add(7);
+        al.add(9);
+        al.add(2);
+        al.add(1);
+        Assert.assertEquals(9, pico.obtener(al));
     }
 
 }
