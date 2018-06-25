28b05c84a9feb93b6c6fe4e2fb7b9edefe82216c
Luciano Nahuel Vitale <lunavi97@gmail.com>
2018-06-25 03:46:51 -0300

    Close #2 - Obtenido el valor del pico y calculada la posición en el vector

--- src/pico/Pico.java
+++ src/pico/Pico.java
@@ -9,7 +9,45 @@ import java.util.PriorityQueue;
  *
  */
 public class Pico {
-    
+
+    /**
+     * 
+     * @param vec - vector que es creciente hasta el pico y luego decrece
+     * @return valor del pico
+     */
+    public Integer solve(int[] vec) {
+        int n = vec.length;
+        int centro, inf = 0, sup = n - 1;
+        while(inf <= sup) {
+            centro = (sup + inf) / 2;
+            if(centro == 0 || centro == n - 1) return vec[centro];
+            else if(vec[centro] < vec [centro - 1]) sup = centro-1;
+            else if(vec[centro] < vec[centro + 1]) inf = centro+1;
+            else return vec[centro];
+        }
+
+        return null;
+    }
+
+    /**
+     * 
+     * @param vec - vector que es creciente hasta el pico y luego decrece
+     * @return posición del pico
+     */
+    public int pos(int[] vec) {
+        int n = vec.length;
+        int centro, inf = 0, sup = n - 1;
+        while(inf <= sup) {
+            centro = (sup + inf) / 2;
+            if(centro == 0 || centro == n - 1) return centro;
+            else if(vec[centro] < vec [centro - 1]) sup = centro - 1;
+            else if(vec[centro] < vec[centro + 1]) inf = centro + 1;
+            else return centro;
+        }
+
+        return -1;
+    }
+
     /**
      * 
      * @param pq - cola de prioridad con enteros en orden descendiente
@@ -18,19 +56,19 @@ public class Pico {
     public int obtener(PriorityQueue<Integer> pq) {
         return pq.peek();
     }
-    
+
     /**
      * 
-     * @param vec - Vector
+     * @param vec - vector
      * @return pico máximo
      */
     public int obtener(int[] vec) {
         PriorityQueue<Integer> pq = new PriorityQueue<Integer>((x, y) -> y - x);
         for(int num : vec) pq.add(num);
-        
+
         return obtener(pq);
     }
-    
+
     /**
      * 
      * @param al - Array List que se ordena de mayor a menor
@@ -40,22 +78,22 @@ public class Pico {
         al.sort((x, y) -> y - x);
         return al.get(0);
     }
-    
+
     /**
      * 
-     * @param vec - Vector creciente hasta pico y luego decreciente 
+     * @param vec - vector creciente hasta pico y luego decreciente 
      * @return entero pico del vector
      */
     public int buscar(int[] vec) {
         if(vec.length == 1) return vec[0];
         if(vec.length == 2) return vec[0] > vec[1] ? vec[0] : vec[1];
-        
+
         int i;
-        
+
         for(i = 1; i < vec.length - 1; i++) {
             if(vec[i] > vec[i - 1] && vec[i] > vec[i + 1]) return vec[i];
         }
-        
+
         return vec[i];
     }
 }
--- test/pico/PicoTests.java
+++ test/pico/PicoTests.java
@@ -8,14 +8,81 @@ import org.junit.Before;
 import org.junit.Test;
 
 public class PicoTests {
-    
+
     Pico pico;
+    int vec[];
+    final int CANT = 10000;
 
     @Before
     public void SetUp() {
         pico = new Pico();
+        vec = new int[CANT];
     }
-    
+
+    @Test (timeout = 5)
+    public void solveOrdenados() {
+        for(int i = 0; i < CANT; i++) {
+            vec[i] = i;
+        }
+        Assert.assertEquals(CANT - 1, (int) pico.solve(vec));
+
+        for(int i = 0; i < CANT; i++) {
+            vec[i] = CANT - i;
+        }
+        Assert.assertEquals(CANT, (int) pico.solve(vec));
+    }
+
+    @Test (timeout = 5)
+    public void solveOrdenadosCrecienteDecreciente() {
+        for(int i = 0; i < (CANT / 2) + 1; i++) {
+            vec[i] = i + 1;
+        }
+
+        for(int i = (CANT / 2) + 1; i < CANT; i++) {
+            vec[i] = CANT - i;
+        }
+
+        Assert.assertEquals((CANT / 2) + 1, (int) pico.solve(vec));
+    }
+
+    @Test (timeout = 5)
+    public void solveDesordenados() {
+        int[] vecS = {4, 8, 10, 11, 5, 4, 3, 2, -1, -2};
+        Assert.assertEquals(11, (int) pico.solve(vecS));
+    }
+
+    @Test (timeout = 5)
+    public void posicionOrdenados() {
+        for(int i = 0; i < CANT; i++) {
+            vec[i] = i;
+        }
+        Assert.assertEquals(CANT - 1, (int) pico.pos(vec));
+
+        for(int i = 0; i < CANT; i++) {
+            vec[i] = CANT - i;
+        }
+        Assert.assertEquals(0, (int) pico.pos(vec));
+    }
+
+    @Test (timeout = 5)
+    public void posicionOrdenadosCrecienteDecreciente() {
+        for(int i = 0; i < (CANT / 2) + 1; i++) {
+            vec[i] = i + 1;
+        }
+
+        for(int i = (CANT / 2) + 1; i < CANT; i++) {
+            vec[i] = CANT - i;
+        }
+
+        Assert.assertEquals(CANT / 2, (int) pico.pos(vec));
+    }
+
+    @Test (timeout = 5)
+    public void posicionDesordenados() {
+        int[] vecP = {1200, 3400, 4123, 2000, 1999, 555, 23, 21};
+        Assert.assertEquals(2, pico.pos(vecP));
+    }
+
     @Test
     public void conColaDePrioridad() {
         PriorityQueue<Integer> pq = new PriorityQueue<Integer>((x, y) -> y - x);
@@ -25,13 +92,13 @@ public class PicoTests {
         pq.add(5);
         Assert.assertEquals(8, pico.obtener(pq));
     }
-    
+
     @Test
     public void conVector() {
         int[] vec = {4, 6, 8, 10, 7, 3, 2, 1};
         Assert.assertEquals(10, pico.obtener(vec));
     }
-    
+
     @Test
     public void conLista() {
         ArrayList<Integer> al = new ArrayList<Integer>();
@@ -42,11 +109,11 @@ public class PicoTests {
         al.add(1);
         Assert.assertEquals(9, pico.obtener(al));
     }
-    
+
     @Test
     public void buscar() {
-        int[] vec = {4, 6, 8, 10, 7, 3, 2, 1};
-        Assert.assertEquals(10, pico.buscar(vec));
+        int[] vecB = {4, 6, 8, 10, 7, 3, 2, 1};
+        Assert.assertEquals(10, pico.buscar(vecB));
     }
 
 }
